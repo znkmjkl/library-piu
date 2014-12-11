efc1bfabd95f3772ca5911a15259866ad879c6d8
Aleksander Krawczyk <alecjo@outlook.com>
2014-12-11 08:48:54 +0100

    Revert "modele"
    This reverts commit d2e3af67c508e7f160c299b7421ae2dfbae1ed03.

--- library/app/models/User.php
+++ library/app/models/User.php
@@ -1,63 +1,74 @@
 <?php
 
-class User extends Eloquent
-{
-	protected $table = 'user';
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableInterface;
 
+class User extends Eloquent implements UserInterface, RemindableInterface {
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'users';
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
 	protected $hidden = array('password');
-	protected $fillable = array('usr_name', 'usr_surname', 'usr_adr_id', 'usr_phone', 'usr_email', 'usr_pesel' );
 
-	public function rental()
+	/**
+	 * Get the unique identifier for the user.
+	 *
+	 * @return mixed
+	 */
+	public function getAuthIdentifier()
 	{
-		return $this->belongsToMany('renta');
+		return $this->getKey();
 	}
 
-	public function reservation()
+	/**
+	 * Get the password for the user.
+	 *
+	 * @return string
+	 */
+	public function getAuthPassword()
 	{
-		return $this->belongsToMany('reservation');
+		return $this->password;
 	}
 
-	public function librarian()
+	/**
+	 * Get the e-mail address where password reminders are sent.
+	 *
+	 * @return string
+	 */
+	public function getReminderEmail()
 	{
-		return $this->belongsTo('librarian');
+		return $this->email;
 	}
 
-	public function address()
+	public function getRememberToken()
 	{
-		return $this->hasOne('address');
+	    return $this->remember_token;
 	}
 
-	public function getIdentifier() { return $this->getKey(); }
-	public function getName()       { return $this->usr_name; }
-	public function getSurname()    { return $this->usr_surname; }
-	public function getAddressId()  { return $this->usr_adr_id; }
-	public function getPhone()      { return $this->usr_phone; }
-	public function getEmail()      { return $this->usr_email; }
-	public function getPassword()   { return $this->usr_password; }
-	public function getNumber()     { return $this->usr_number; }
-	public function getPesel()      { return $this->usr_pesel; }
-	public function getActive()     { return $this->usr_active; }
-	public function getVerified()   { return $this->usr_verified; }
-	
-	public function setName($value)       { $this->usr_name = $value; }
-	public function setSurname($value)    { $this->usr_surname = $value; }
-	public function setAddressId($value)  { $this->usr_adr_id = $value; }
-	public function setPhone($value)      { $this->usr_phone = $value; }
-	public function setEmail($value)      { $this->usr_email = $value; }
-	public function setPassword($value)   { $this->usr_password = $value; }
-	public function setNumber($value)     { $this->usr_number = $value; }
-	public function setPesel($value)      { $this->usr_pesel = $value; }
-	public function setActive($value)     { $this->usr_active = $value; }
-	public function setVerified($value)   { $this->usr_verified = $value; }
+	public function setRememberToken($value)
+	{
+	    $this->remember_token = $value;
+	}
 
-	public static $rules = array('usr_name'=>'required|alpha|between:2, 30',
-				     'usr_surname'=>'required|alpha|between:2, 30',
-				     'usr_adr_id'=>'required|integer|min:0',
-				     'usr_phone'=>'required|alpha|between:9, 12',
-				     'usr_email'=>'required|emailconfirmed',
-				     'usr_email_confirmation'=>'required|email',
-				     'usr_password'=>'required|aplha_num|between:8,30',
-				     'usr_pesel'=>'required|alpha|between:9,9'
-				    );
-}
-?>
+	public function getRememberTokenName()
+	{
+	    return 'remember_token';
+	}
+
+
+	public static $rules = array('firstname'=>'required|alpha|min:2',
+								 'lastname'=>'required|alpha|min:2',
+								 'email'=>'required|email|unique:users',
+								 'password'=>'required|alpha_num|between:6,12|confirmed',
+								 'password_confirmation'=>'required|alpha_num|between:6,12');
+
+}#komentarz jakis
